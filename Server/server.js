22ace996c308ae95cf0ffa7353febd4f3cca837f
niyatiit <prayaggohil77@gmai.com>
2026-05-03 23:46:10 +0530

    Update CORS config and API endpoints for deployed backend

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://spotify-clone-f395.onrender.com"],
     credentials: true,
   }),
 );
